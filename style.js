ea5013dd2c4480b3da35be4a82c49760ac049557
umar ahmed <umarkhan03102520914@gmail.com>
2025-12-30 16:14:26 +0500

    complete

--- style.js
+++ style.js
@@ -1,26 +1,30 @@
-let Student_Name="Umer Ahmed"
-let Class="10th"
-let Total=900
-let Age =21
-let percentage=80
-let Grade= ""
+let Student_Name = prompt("Enter The Student Name");
+let Class = prompt("Enter the Class");
+let TotalMarks = 100;
+let ObtainedMarks = parseFloat(prompt("Enter the Obtained Marks"));
+let Age = prompt("Enter the age");
+let Grade = "";
 
+let percentage = (ObtainedMarks / TotalMarks) * 100;
 
-if(percentage>=80){
-Grade="A+"
-}else if(percentage>=70){
-Grade="A"
-}else if(percentage>=60){
-Grade="B"
-}else if(percentage>=50){
-Grade="c"
-}else{
-    Grade="Failed"
+if (percentage >= 80) {
+    Grade = "A+";
+} else if (percentage >= 70) {
+    Grade = "A";
+} else if (percentage >= 60) {
+    Grade = "B";
+} else if (percentage >= 50) {
+    Grade = "C";
+} else {
+    Grade = "Failed";
 }
-alert("Student_Name :" + Student_Name+"\n" +
-    "Class :"+Class+"\n"+
-    "Total :" +Total + "\n"+
-    "Age :"+Age+"\n"+
-    "percentage :"+percentage+"\n"+
-    "Grade :"+Grade
-)
+
+alert(
+    "Student Name: " + Student_Name + "\n" +
+    "Class: " + Class + "\n" +
+    "Total Marks: " + TotalMarks + "\n" +
+    "Obtained Marks: " + ObtainedMarks + "\n" +
+    "Age: " + Age + "\n" +
+    "Percentage: " + percentage + "%\n" +
+    "Grade: " + Grade
+);
